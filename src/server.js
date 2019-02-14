e00ff1b0520971da5619573421515984712fcb54
Marc Eastman <eastman@umd.edu>
2019-02-13 19:30:34 -0500

    goddddd

--- src/server.js
+++ src/server.js
@@ -8,19 +8,25 @@ app.use(express.json());
 
 app.post("/newsong", (req,res) => {
   if(req.body.sender_type === "bot") return;
-  console.log(req.body);
-  const text = "yooooo";
-  axios.post("https://api.groupme.com/v3/bots/post",
-  {
-    bot_id: process.env.BOT_ID,
-    text: JSON.stringify(req.body.text)
-  }).then(function (response) {
-    return res.send("all good" + JSON.stringify(req.body));
-  })
-  .catch(function (error) {
-    return res.send("failure" + error);
+  const scRegex = /.*soundcloud\.com.*/;
+  if(scRegex.match(req.body.text)){
+    const text = "yooooo";
+    axios.post("https://api.groupme.com/v3/bots/post",
+    {
+      bot_id: process.env.BOT_ID,
+      text: JSON.stringify(req.body.text)
+    }).then(function (response) {
+      return res.send("all good" + JSON.stringify(req.body));
+    })
+    .catch(function (error) {
+      return res.send("failure" + error);
+    });
   });
-});
+  }
+
+  console.log(req.body.text);
+  // if you want to send a message back to group
+
 
 let port = process.env.PORT;
 if (port == null || port == "") {
