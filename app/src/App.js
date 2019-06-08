18cedc6bcfc2a5dfbf91aad282e3355e15e53739
Marc Eastman <eastman@umd.edu>
2019-06-07 23:28:53 -0400

    updated frontend

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const listOfLinks = [...new Array(100)].map(ele => Math.random().toString(36));
+
   return (
     <div className="App">
+      <div class="card header">
+        <div class="card-body">
+          <h5 class="card-title">Welcome Fam</h5>
+          <a href="#" class="btn btn-success">Spotify Playlist</a>
+        </div>
+      </div>
       <div class="card">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 class="card-title">Soundcloud Posts</h5>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Date Posted</th>
+                <th scope="col">Link</th>
+                <th scope="col">Creds</th>
+              </tr>
+            </thead>
+            <tbody>
+              {listOfLinks.map((url, index) =>
+                <tr>
+                  <th scope="row">{index+1}</th>
+                  <td>
+                    {url}
+                  </td>
+                  <td>
+                    {url}
+                  </td>
+                  <td>
+                    {url}
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
